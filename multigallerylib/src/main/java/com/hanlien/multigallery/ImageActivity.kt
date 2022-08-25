8677840a47b6aa17080ba41ffe05dc20b9f3f7b5
hanseulchoi <lotte.hanseul@gmail.com>
2022-08-25 17:23:36 +0900

    init view

--- multigallerylib/src/main/java/com/hanlien/multigallery/ImageActivity.kt
+++ multigallerylib/src/main/java/com/hanlien/multigallery/ImageActivity.kt
@@ -1,7 +1,17 @@
 package com.hanlien.multigallery
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.hanlien.multigallery.databinding.ActivityImageBinding
 
 class ImageActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityImageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_image)
+    }
 }
