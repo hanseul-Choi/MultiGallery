e41f05ac95f3ef7e2b725be2a0220bdbfa1b9149
hanseulchoi <lotte.hanseul@gmail.com>
2022-08-26 08:40:25 +0900

    feat : Sample View

--- sample/src/main/java/com/hanlien/sample/MainActivity.kt
+++ sample/src/main/java/com/hanlien/sample/MainActivity.kt
@@ -2,10 +2,20 @@ package com.hanlien.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.hanlien.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.testBtn.setOnClickListener {
+            Toast.makeText(this, "on click", Toast.LENGTH_SHORT).show()
+        }
     }
 }
