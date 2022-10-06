92edfb0212ec8f8448198455086a35c542230415
hanseulchoi <lotte.hanseul@gmail.com>
2022-10-06 16:47:14 +0900

    refact : Delete No Use Code in imageGetter class

--- multigallerylib/src/main/java/com/hanlien/multigallery/ImageGetter.kt
+++ multigallerylib/src/main/java/com/hanlien/multigallery/ImageGetter.kt
@@ -8,17 +8,11 @@ import androidx.annotation.RequiresApi
 
 class ImageGetter(private val resolver: ContentResolver?) {
     fun getImageCursor(album: String): Cursor {
-        var queryUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-
         val what = arrayOf(
             MediaStore.Images.ImageColumns._ID,
             MediaStore.Images.ImageColumns.TITLE,
             MediaStore.Images.ImageColumns.DATA
         )
-
-        val orderBy = MediaStore.Images.ImageColumns.DATE_TAKEN + " DESC"
-
-        queryUri = queryUri.buildUpon().appendQueryParameter("limit", "1").build()
 
         return resolver?.query(
             MediaStore.Images.Media.EXTERNAL_CONTENT_URI, what, MediaStore.Images.Media.BUCKET_DISPLAY_NAME + " =?", arrayOf(album), MediaStore.Images.Media.DATE_ADDED)!!
